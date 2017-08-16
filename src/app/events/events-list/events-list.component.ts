c6aab45c27e43901ae8ca15ad0675778dc114b42
vladimirzinyak <dante8919@yandex.ru>
2017-08-16 18:06:39 +0300

    swibo

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -23,14 +23,13 @@ export class EventsListComponent implements OnInit {
   public skills: any[] = [];
   public sillsInfo;
 
-  private myDateRangePickerOptions: IMyDrpOptions = {
-    // other options...
+  public myDateRangePickerOptions: IMyDrpOptions = {
     dateFormat: 'dd.mm.yyyy',
   };
 
   // For example initialize to specific date (09.10.2018 - 19.10.2018). It is also possible
   // to set initial date range value using the selDateRange attribute.
-  private model: Object = {beginDate: {year: 2018, month: 10, day: 9},
+  public model: Object = {beginDate: {year: 2018, month: 10, day: 9},
     endDate: {year: 2018, month: 10, day: 19}};
 
   constructor(public eventsService: EventsService,
